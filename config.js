385c4ce89e067330149fe4647aafb191dd1a5506
borsTiHD <bastian.j@gmx.net>
2021-05-07 12:40:03 +0200

    🎨 Config >> Replace with conf module.

---
+++ config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    //nicehash config
+    apiConf: {
+        apiHost: 'https://api2.nicehash.com', //use https://api2.nicehash.com for production
+        apiKey: 'x', //get it here: https://test.nicehash.com/my/settings/keys or https://new.nicehash.com/my/settings/keys
+        apiSecret: 'x',
+        orgId: 'x',
+    },
+    //nicehash Wallet address
+    btcAddress: 'x'
+}
