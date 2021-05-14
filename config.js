a21dbf68464e95b6337a7fe984b040a1e411ba7b
Henry Williams <github@digitalhen.com>
2021-05-13 20:37:12 -0400

    adds api

--- config.js
+++ config.js
@@ -2,15 +2,15 @@ module.exports = {
     //nicehash config
     apiConf: {
         apiHost: 'https://api2.nicehash.com', //use https://api2.nicehash.com for production
-        apiKey: 'x', //get it here: https://test.nicehash.com/my/settings/keys or https://new.nicehash.com/my/settings/keys
-        apiSecret: 'x',
-        orgId: 'x',
+        apiKey: '4373b6c5-35e8-4e39-8cb4-ee1ff309b187', //get it here: https://test.nicehash.com/my/settings/keys or https://new.nicehash.com/my/settings/keys
+        apiSecret: '2a713ef0-1f2e-4897-9364-92521763cc608ef6d5e4-623b-4d52-a281-a4215827160f',
+        orgId: '1932ab9c-58f2-4eb4-96a8-06a9c12da98a',
     },
     coinbaseConf: {
 	apiHost: 'https://api.coinbase.com',
-	apiKey: 'x',
-	apiSecret: 'x'
+	apiKey: 'wEOvvgR41imVzhY1',
+	apiSecret: 'M0SlJ0zL78ceicXcUa3JtEYwHP0Jzas3'
     },
     //nicehash Wallet address
-    btcAddress: 'x'
+    btcAddress: '3FkZUiDgcE3jSwe2CnyiL6bR4Rz3rj3Gtx'
 }
